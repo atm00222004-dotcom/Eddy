225a31e9e5ccbc9e485557a3d883d26d8f1e0648
DESKTOP-K586V53\DELL <jaspreet98770@gmail.com>
2026-03-27 16:25:01 +0530

    seq script

---
+++ Eddy/Scripts/seq.sql
@@ -0,0 +1,16 @@
+-- SEQUENCE: public.logs_Id_seq
+
+-- DROP SEQUENCE IF EXISTS public."logs_Id_seq";
+
+CREATE SEQUENCE IF NOT EXISTS public."logs_Id_seq"
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+ALTER SEQUENCE public."logs_Id_seq"
+    OWNED BY public."Logs"."Id";
+
+ALTER SEQUENCE public."logs_Id_seq"
+    OWNER TO postgres;
